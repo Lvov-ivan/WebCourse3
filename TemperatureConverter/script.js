690e8cbe86ff3e93c90738863cb2500ba70062ca
RTFM <lvov-ivan@yandex.ru>
2025-01-02 15:52:53 +0700

    Правки по задаче "Конвертер температуры"

--- TemperatureConverter/script.js
+++ TemperatureConverter/script.js
@@ -1,12 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-    document.getElementById("convertButton").addEventListener("click", () => {
-        const celsiusTemperature = document.getElementById("input").value;
+    document.getElementById("convert-button").addEventListener("click", () => {
+        const celsiusTemperature = document.getElementById("input-celsius-field").value;
 
         if (isNaN(celsiusTemperature)) {
-            alert("Введите число в поле \"Температура в цельсиях\"")
+            alert("Введите число в поле \"Температура в цельсиях\"");
         } else {
-            document.getElementById("kelvinResult").value = +celsiusTemperature + 273.15;
-            document.getElementById("fahrenheitResult").value = +celsiusTemperature * 1.8 + 32;
+            document.getElementById("kelvin-result").value = convertToKelvin(celsiusTemperature);
+            document.getElementById("fahrenheit-result").value = convertToFahrenheit(celsiusTemperature);
         }
-    })
-})
+    });
+});
+
+function convertToKelvin(celsiusTemperature) {
+    return +celsiusTemperature + 273.15;
+}
+
+function convertToFahrenheit(celsiusTemperature) {
+    return +celsiusTemperature * 1.8 + 32;
+}
